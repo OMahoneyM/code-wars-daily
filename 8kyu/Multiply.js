fb28aee4e06fa7ddb744cbf26a4754d40ac6e188
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-01-05 11:35:48 -0500

    Added daily challenge

---
+++ 8kyu/Multiply.js
@@ -0,0 +1,10 @@
+// Description:
+// --------------------------------------
+// This code does not execute properly. Try to figure out why.
+
+
+// Solution:
+// --------------------------------------
+const multiply = (a, b) => a * b;
+
+console.log(multiply(5, 10))
