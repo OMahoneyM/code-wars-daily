3d997677dc29dbbda5ab18aadd68fdfca8e81fcd
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-01-28 12:36:08 -0500

    Added daily challenge

---
+++ 8kyu/Remove duplicates from list.js
@@ -0,0 +1,27 @@
+// Description:
+// --------------------------------------
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+
+// Solution:
+// --------------------------------------
+function distinct(a) {
+    return a.filter((e, i, arr) => arr.indexOf(e) === i);
+}
+
+console.log(distinct([1,2,1,1,3,2])) //[1,2,3]
+
+
+// Alt Solution:
+// --------------------------------------
+function distinct(a) {
+    return [...new Set(a)];
+}
