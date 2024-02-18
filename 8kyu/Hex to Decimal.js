b92bc45c1959d9fd18613d73fa0fbbd400d26b2d
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-02-18 12:17:56 -0500

    Added daily challenge

---
+++ 8kyu/Hex to Decimal.js
@@ -0,0 +1,10 @@
+// Description:
+// --------------------------------------
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+// Solution:
+// --------------------------------------
+const hexToDec = hexStr => parseInt(hexStr, 16);
+
+console.log(hexToDec("FF")) //255
