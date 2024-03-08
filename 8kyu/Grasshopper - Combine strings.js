002aaa436ec40cfbde8e7b375629468281e56204
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-03-08 11:47:01 -0500

    Added daily challenge

---
+++ 8kyu/Grasshopper - Combine strings.js
@@ -0,0 +1,20 @@
+// Description:
+// --------------------------------------
+// Combine strings function
+
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+
+// returns:
+
+// 'James Stevens'
+
+
+// Solution:
+// --------------------------------------
+const combineNames = (fName, lName) => fName + ' ' + lName;
+
+console.log(combineNames('Heywood', 'Yabuzoff')) //Heywood Yabuzoff
