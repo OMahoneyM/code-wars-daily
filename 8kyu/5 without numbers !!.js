c0c45f16ec37e9e2513eb7a55733690f32a7fecd
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-03-21 11:22:19 -0500

    Added daily challenge

---
+++ 8kyu/5 without numbers !!.js
@@ -0,0 +1,16 @@
+// Description:
+// --------------------------------------
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+
+// Solution:
+// --------------------------------------
+function unusualFive() {
+    return 'testy'.length;
+  }
+
+console.log(unusualFive())
