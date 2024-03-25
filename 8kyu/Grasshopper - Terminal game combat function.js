e18c9f938bb71218abf8cc3c21b7e639e3534f12
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-03-25 11:14:30 -0500

    Added daily challenge

---
+++ 8kyu/Grasshopper - Terminal game combat function.js
@@ -0,0 +1,13 @@
+// Description:
+// --------------------------------------
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+
+// Solution:
+// --------------------------------------
+function combat(health, damage){
+    return (health > damage) ? health - damage : 0;
+}
+
+console.log(combat(100, 40)) //60
+console.log(combat(20, 40)) //0
