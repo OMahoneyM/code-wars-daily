5801c3a10d20e50074b64659b96afbcb49b17d26
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-03-26 11:56:28 -0500

    Added daily challenge

---
+++ 8kyu/Bin to Decimal.js
@@ -0,0 +1,13 @@
+// Description:
+// --------------------------------------
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+
+// Solution:
+// --------------------------------------
+const binToDec = bin => parseInt(bin, 2);
+
+console.log(binToDec('0')) //0
+console.log(binToDec('1')) //1
+console.log(binToDec('10')) //2
+console.log(binToDec('1001001')) //73
