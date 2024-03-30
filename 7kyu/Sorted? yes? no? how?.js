63577e8b6ea7af703f73b837b566faf2f402d8b0
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-03-30 11:20:17 -0500

    Added daily challenge

---
+++ 7kyu/Sorted? yes? no? how?.js
@@ -0,0 +1,21 @@
+// Description:
+// --------------------------------------
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+//     "yes, ascending" - if the numbers in the array are sorted in an ascending order
+//     "yes, descending" - if the numbers in the array are sorted in a descending order
+//     "no" - otherwise
+
+// You can assume the array will always be valid, and there will always be one correct answer.
+
+
+// Solution:
+// --------------------------------------
+function isSortedAndHow(arr){
+  return arr.every((x, i) => i == 0 || arr[i] >= arr[i-1]) ? 'yes, ascending' :
+         arr.every((x, i) => i == 0 || arr[i] <= arr[i-1]) ? 'yes, descending' : 'no'
+}
+
+console.log(isSortedAndHow([1,2])) //'yes, ascending'
+console.log(isSortedAndHow([15, 7, 3, -8])) //'yes, descending'
+console.log(isSortedAndHow([20, 3, 4])) //'no'
