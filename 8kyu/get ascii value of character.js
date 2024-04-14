0ba91a9173c72331de351ae9676b7915d4fe8454
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-04-14 12:36:44 -0500

    Added daily challenge

---
+++ 8kyu/get ascii value of character.js
@@ -0,0 +1,12 @@
+// Description:
+// --------------------------------------
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+
+// Solution:
+// --------------------------------------
+const getASCII = c => c.charCodeAt(0);
+
+console.log(getASCII('A')) //65
