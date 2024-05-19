e68f911a556afa6e036043f36bd05595556f4679
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-05-18 23:33:25 -0400

    Added daily challenge

---
+++ 6kyu/Sort the odd.js
@@ -0,0 +1,20 @@
+// Description:
+// -------------------------------------- 
+// You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+// Examples
+
+// [7, 1]  =>  [1, 7]
+// [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+// [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+
+// Solution:
+// --------------------------------------
+function sortArray(array) {
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+}
+
+console.log(sortArray([5, 8, 6, 3, 4]))
+
+// 0 is actually a falsy value and 1 is truthy. So when numbers hit x % 2 the remainder operator returns 1 for odd numbers which is viewed as true, and returns 0 for even numbers which is viewed as false. 
