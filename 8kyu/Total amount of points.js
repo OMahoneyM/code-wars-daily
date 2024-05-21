c269125d0fd8b03723e1298845c1312c394c84fb
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-05-21 11:53:07 -0400

    Added daily challenge

---
+++ 8kyu/Total amount of points.js
@@ -0,0 +1,53 @@
+// Description:
+// -------------------------------------- 
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+//     if x > y: 3 points (win)
+//     if x < y: 0 points (loss)
+//     if x = y: 1 point (tie)
+
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+//     our team always plays 10 matches in the championship
+//     0 <= x <= 4
+//     0 <= y <= 4
+
+
+// Solution:
+// --------------------------------------
+let testCase = ["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]
+
+function points(games) {
+    let total = 0
+    games.forEach(game => {
+        const [x,y] = game.split(':').map(Number)
+
+        if (x > y){
+            total += 3
+        }else if (x < y){
+            total += 0
+        }else{
+            total += 1
+        }
+    })
+
+    return total
+}
+
+console.log(points(testCase)) // 30
+
+// Better Solution:
+// --------------------------------------
+const points = games => games.reduce((accum, current) => {
+    return accum += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0
+}, 0)
+
+console.log(points(testCase))
