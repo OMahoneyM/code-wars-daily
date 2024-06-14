21d01f171d2800d40457c60975f5cf95f9d60af6
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-14 10:15:21 -0400

    Added daily challenge

---
+++ 8kyu/Function 2 - squaring an argument.js
@@ -0,0 +1,8 @@
+// Description:
+// --------------------------------------
+// Now you have to write a function that takes an argument and returns the square of it.
+
+
+// Solution:
+// --------------------------------------
+const square = n => Math.pow(n, 2);
