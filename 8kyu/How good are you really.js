3c5437276ecc42efbaace71ac0a0112d36b478fb
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-15 14:44:46 -0400

    Added daily challenge

---
+++ 8kyu/How good are you really.js
@@ -0,0 +1,22 @@
+// Description:
+// --------------------------------------
+// There was a test in your class and you passed it. Congratulations!
+
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+// You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+// Return true if you're better, else false!
+// Note:
+
+// Your points are not included in the array of your class's points. Do not forget them when calculating the average score!
+
+
+// Solution:
+// --------------------------------------
+function betterThanAverage(classPoints, yourPoints) {
+    return yourPoints > ((classPoints.reduce((a, c) => a + c, yourPoints)) / (classPoints.length + 1))
+}
+
+console.log(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)) // true
+console.log(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)) // false
