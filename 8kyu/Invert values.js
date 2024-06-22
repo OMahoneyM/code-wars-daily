0cb085c3103e463c769d4f783d330c55a67e53e3
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-22 12:37:32 -0400

    Added daily challenge

---
+++ 8kyu/Invert values.js
@@ -0,0 +1,17 @@
+// Description:
+// --------------------------------------
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// [1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+// [1, -2, 3, -4, 5] --> [-1, 2, -3, 4, -5]
+// [] --> []
+
+// You can assume that all values are integers. Do not mutate the input array.
+
+
+// Solution:
+// --------------------------------------
+const invert = array => array.map(e => e * -1);
+
+console.log(invert([1,2,3,4,5]))
+console.log(invert([1,-2,3,-4,5]))
