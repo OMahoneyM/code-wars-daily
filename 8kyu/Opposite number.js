d837f539d9fd40d6dc602ede61b59a5792743d39
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-30 21:39:58 -0400

    Added daily challenge

---
+++ 8kyu/Opposite number.js
@@ -0,0 +1,16 @@
+// Description:
+// --------------------------------------
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+// Solution:
+// --------------------------------------
+const opposite = number => -number;
+
+console.log(opposite(14))
+console.log(opposite(-20))
