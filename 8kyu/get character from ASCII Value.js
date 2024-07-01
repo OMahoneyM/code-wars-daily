3195e7d216e4b5a8cc7ec96986470635c196184a
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-06-30 21:59:26 -0400

    Added daily challenge

---
+++ 8kyu/get character from ASCII Value.js
@@ -0,0 +1,18 @@
+// Description:
+// --------------------------------------
+// Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+// Example:
+
+// 65 --> 'A'
+// 97 --> 'a'
+// 48 --> '0
+
+// For ASCII table, you can refer to http://www.asciitable.com/
+
+
+// Solution:
+// --------------------------------------
+const getChar = c => String.fromCharCode(c);
+
+console.log(getChar(65)) //A
