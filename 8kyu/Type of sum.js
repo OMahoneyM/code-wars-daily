552226571964bfe6b4e3c6fa8354a88f296e2acd
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-08-03 11:08:23 -0500

    Added daily challenge

---
+++ 8kyu/Type of sum.js
@@ -0,0 +1,8 @@
+// Description:
+// --------------------------------------
+// Return the type of the sum of the two arguments
+
+
+// Solution:
+// --------------------------------------
+const typeOfSum = (a, b) => typeof (a + b);
