b0c0ab1577d778ffb0b5f8b24bfb1c3cd142e66f
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-08-13 12:08:19 -0500

    Added alternate solution

--- 7kyu/The Office I - Outed.js
+++ 7kyu/The Office I - Outed.js
@@ -29,3 +29,13 @@ function outed(meet, boss){
 
 console.log(outed({'tim':0, 'jim':2, 'randy':0, 'sandy':7, 'andy':0, 'katie':5, 'laura':1, 'saajid':2, 'alex':3, 'john':2, 'mr':0}, 'laura')) //'Get Out Now!'
 console.log(outed({'tim':1, 'jim':3, 'randy':9, 'sandy':6, 'andy':7, 'katie':6, 'laura':9, 'saajid':9, 'alex':9, 'john':9, 'mr':8}, 'katie')) //'Nice Work Champ!'
+
+
+// Alt Solution:
+// --------------------------------------
+function outed(meet, boss) {
+    let names = Object.keys(meet);
+    let score = names.reduce((acc, cur) => acc + meet[cur], 0) + meet[boss];
+
+    return score / names.length <= 5 ? 'Get Out Now!' : 'Nice Work Champ!';
+}
