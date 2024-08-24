74006063c28f95efb4075dda4ef914a9d21df0c8
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-08-24 12:03:42 -0500

    Added daily challenge

---
+++ 8kyu/Return to Sanity.js
@@ -0,0 +1,13 @@
+// Description:
+// --------------------------------------
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+
+// Solution:
+// --------------------------------------
+function mystery() {
+    let results = {sanity: 'Hello'};
+    return results;
+}
+
+console.log(mystery())
