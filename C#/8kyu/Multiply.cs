09254c3180c0beed8437ae2f48fb8dcb12171b29
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-09-12 11:03:47 -0500

    Added daily challenge

---
+++ C#/8kyu/Multiply.cs
@@ -0,0 +1,12 @@
+// Description:
+// --------------------------------------
+// This code does not execute properly. Try to figure out why.
+
+
+// Solution:
+// --------------------------------------
+public class CustomMath {
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+}
