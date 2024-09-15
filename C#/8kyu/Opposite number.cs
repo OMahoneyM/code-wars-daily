218d15389acc2f3a01af2535f18136004f9f834f
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-09-15 11:11:13 -0500

    Added daily challenge

---
+++ C#/8kyu/Opposite number.cs
@@ -0,0 +1,22 @@
+// Description:
+// --------------------------------------
+// Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+
+// Solution:
+// --------------------------------------
+using System;
+
+public class Kata
+    {
+        public  static int Opposite(int number)
+        {
+          return number * -1;
+        }
+    }
