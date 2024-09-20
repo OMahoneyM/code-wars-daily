28a4c0b0781c21ebd6f014356c430c647dfd5861
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-09-20 11:23:17 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/ToLeetSpeak.js
@@ -0,0 +1,86 @@
+// Description:
+// --------------------------------------
+// Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
+
+// More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
+
+// Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+
+// For example:
+
+// toLeetSpeak("LEET") returns "1337"
+
+// In this kata we use a simple LeetSpeak dialect. Use this alphabet:
+
+// {
+//   A : '@',
+//   B : '8',
+//   C : '(',
+//   D : 'D',
+//   E : '3',
+//   F : 'F',
+//   G : '6',
+//   H : '#',
+//   I : '!',
+//   J : 'J',
+//   K : 'K',
+//   L : '1',
+//   M : 'M',
+//   N : 'N',
+//   O : '0',
+//   P : 'P',
+//   Q : 'Q',
+//   R : 'R',
+//   S : '$',
+//   T : '7',
+//   U : 'U',
+//   V : 'V',
+//   W : 'W',
+//   X : 'X',
+//   Y : 'Y',
+//   Z : '2'
+// }
+
+
+// Solution:
+// --------------------------------------
+function toLeetSpeak(str) {
+let leetDic = {
+    A : '@',
+    B : '8',
+    C : '(',
+    D : 'D',
+    E : '3',
+    F : 'F',
+    G : '6',
+    H : '#',
+    I : '!',
+    J : 'J',
+    K : 'K',
+    L : '1',
+    M : 'M',
+    N : 'N',
+    O : '0',
+    P : 'P',
+    Q : 'Q',
+    R : 'R',
+    S : '$',
+    T : '7',
+    U : 'U',
+    V : 'V',
+    W : 'W',
+    X : 'X',
+    Y : 'Y',
+    Z : '2'
+  }
+
+  let result = '';
+  
+  for (let letter of str) {
+    letter in leetDic ? result += leetDic[letter] : result += letter; 
+  }
+
+  return result;
+}
+
+console.log(toLeetSpeak('LEET')) //1337
