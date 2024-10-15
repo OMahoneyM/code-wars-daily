e49b43db9dae3a5baa18632b2933b76f0714aba0
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-10-15 10:31:18 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Sort arrays - 1.js
@@ -0,0 +1,10 @@
+// Description:
+// --------------------------------------
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+// Solution:
+// --------------------------------------
+const sortme = arr => arr.sort();
+
+console.log(sortme(["one", "two", "three"]), ["one", "three", "two"])
