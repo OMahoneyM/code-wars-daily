9bedc10503f0c7109b5f81d86038b09597c42101
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-10-16 10:25:58 -0500

    Added alternate solution

--- JavaScript/7kyu/Reverse list.js
+++ JavaScript/7kyu/Reverse list.js
@@ -9,3 +9,17 @@ const reverseList = arr => arr.reverse();
 
 console.log(reverseList([]), [])
 console.log(reverseList([1,2,3]), [3,2,1])
+
+
+// Alt Solution:
+// --------------------------------------
+function reverseList(arr){
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result.shift(arr[i]);
+    }
+    return result;
+}
+
+console.log(reverseList([]), [])
+console.log(reverseList([1,2,3]), [3,2,1])
