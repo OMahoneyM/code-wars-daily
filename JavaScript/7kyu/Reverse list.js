31143179a599c9a7838f6b412964e45f029972b0
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-10-16 10:22:03 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Reverse list.js
@@ -0,0 +1,11 @@
+// Description:
+// --------------------------------------
+// Write reverseList function that simply reverses lists.
+
+
+// Solution:
+// --------------------------------------
+const reverseList = arr => arr.reverse();
+
+console.log(reverseList([]), [])
+console.log(reverseList([1,2,3]), [3,2,1])
