a21a444ef3d27f7d2e0f78455c8f85b77dc99e79
Michael O'Mahoney <mjvomahoney@gmail.com>
2024-10-26 11:19:53 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Incorrect division method.js
@@ -0,0 +1,10 @@
+// Description:
+// --------------------------------------
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+
+// Solution:
+// --------------------------------------
+const solve = (x, y) => x / y;
+
+console.log(solve(10, 5), 2)
