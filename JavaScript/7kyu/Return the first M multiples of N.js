52ea6273e3fddb1c4a0c36dee6ebaa5330a2d370
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-02-07 11:52:43 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Return the first M multiples of N.js
@@ -0,0 +1,20 @@
+// Description:
+// --------------------------------------
+// Implement a function that takes two numbers m and n and returns an array of the first m multiples of the real number n. Assume that m is a positive integer.
+
+// Ex.
+
+// (3, 5.0) --> [5.0, 10.0, 15.0]
+
+
+// Solution:
+// --------------------------------------
+function multiples(m, n){
+    let result = []
+    for(let i = 1; i <= m; i++){
+      result.push(n * i)
+    }
+    return result
+}
+
+console.log(multiples(3, 5))
