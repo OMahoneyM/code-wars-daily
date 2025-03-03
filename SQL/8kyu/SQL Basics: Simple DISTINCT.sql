f12d2be66eac1967c7db08fd570e580759f21961
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-03-02 22:45:23 -0500

    Added SQL challenge

---
+++ SQL/8kyu/SQL Basics: Simple DISTINCT.sql
@@ -0,0 +1,17 @@
+-- Description:
+-- --------------------------------------
+-- For this challenge you need to create a simple DISTINCT statement, you want to find all the unique ages.
+-- people table schema
+
+--     id
+--     name
+--     age
+
+-- select table schema
+
+--     age (distinct)
+
+
+-- Solution:
+-- --------------------------------------
+SELECT DISTINCT age FROM people;
