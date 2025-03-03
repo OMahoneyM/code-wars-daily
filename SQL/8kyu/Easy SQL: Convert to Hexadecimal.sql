5c3d2ccbfffe83798182e89a1bae4d202661ef24
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-03-03 11:46:40 -0500

    Added SQL challenge

---
+++ SQL/8kyu/Easy SQL: Convert to Hexadecimal.sql
@@ -0,0 +1,22 @@
+-- Description:
+-- --------------------------------------
+-- to hexYou have access to a table of monsters as follows:
+-- monsters table schema
+
+--     id
+--     name
+--     legs
+--     arms
+--     characteristics
+
+-- Your task is to turn the numeric columns (arms, legs) into equivalent hexadecimal values.
+-- output table schema
+
+--     legs
+--     arms
+
+
+-- Solution:
+-- --------------------------------------
+SELECT TO_HEX(legs) AS legs, TO_HEX(arms) AS arms
+FROM monsters;
