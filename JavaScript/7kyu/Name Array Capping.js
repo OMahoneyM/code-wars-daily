5e366011dd635ee3e09ab06248de25a1d71c9819
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-06-17 10:23:30 -0400

    Added daily challenge

---
+++ JavaScript/7kyu/Name Array Capping.js
@@ -0,0 +1,17 @@
+// Description:
+// --------------------------------------
+// Create a function that accepts an array of names, and returns an array of each name with its first letter capitalized and the remainder in lowercase.
+// Examples
+
+// ["jo", "nelson", "jurie"] -->  ["Jo", "Nelson", "Jurie"]
+// ["KARLY", "DANIEL", "KELSEY"] --> ["Karly", "Daniel", "Kelsey"]
+
+
+// Solution:
+// --------------------------------------
+function capMe(names) {
+    return names.map(e => e[0].toUpperCase() + e.slice(1).toLowerCase())
+}
+
+console.log(capMe(["jo", "nelson", "jurie"]), ["Jo", "Nelson", "Jurie"])
+console.log(capMe(["KARLY", "DANIEL", "KELSEY"]), ["Karly", "Daniel", "Kelsey"])
