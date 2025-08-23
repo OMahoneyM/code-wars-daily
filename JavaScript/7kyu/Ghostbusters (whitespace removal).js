e71e6350fafdb8cb05cf30db2e4d28d3c324386f
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-08-23 14:43:31 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Ghostbusters (whitespace removal).js
@@ -0,0 +1,25 @@
+// Description:
+// --------------------------------------
+// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+// Example:
+
+// "Sky scra per" -> "Skyscraper"
+
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+
+// Solution:
+// --------------------------------------
+function ghostBusters(building) {
+  let result = building.replace(/\s/g,"");
+  return (building === result) ? "You just wanted my autograph didn't you?" : result;
+}
+
+console.log(ghostBusters("Sky scra per"), "Skyscraper")
