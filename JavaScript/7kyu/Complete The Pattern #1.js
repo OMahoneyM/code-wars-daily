369e891f76bdcd281e50d5010d84a12a4f3c6c7e
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-09-06 10:58:06 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Complete The Pattern #1.js
@@ -0,0 +1,64 @@
+// Description:
+// --------------------------------------
+// Task:
+
+// You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+//     Note:Returning the pattern is not the same as Printing the pattern.
+
+// Rules/Note:
+
+//     If n < 1 then it should return "" i.e. empty string.
+//     There are no whitespaces in the pattern.
+
+// Pattern:
+
+// 1
+// 22
+// 333
+// ....
+// .....
+// nnnnnn
+
+// Examples:
+
+//     pattern(5):
+
+//     1
+//     22
+//     333
+//     4444
+//     55555
+
+//     pattern(11):
+
+//     1
+//     22
+//     333
+//     4444
+//     55555
+//     666666
+//     7777777
+//     88888888
+//     999999999
+//     10101010101010101010
+//     1111111111111111111111
+
+//     Hint: Use \n in string to jump to next line
+
+
+// Solution:
+// --------------------------------------
+function pattern(n){
+    let result = [];
+
+    for(let i = 1; i <= n; i++){
+        result.push(String(i).repeat(i));
+    }
+
+    return result.join("\n");
+}
+
+console.log(pattern(-1), "")
+console.log(pattern(0), "")
+console.log(pattern(3), "1\n22\n333")
