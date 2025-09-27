5e592a1c5878f1fee648889359dfcbac94315b07
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-09-27 10:24:29 -0500

    Added daily challenge

---
+++ JavaScript/7kyu/Santa's Naughty List.js
@@ -0,0 +1,16 @@
+// Description:
+// --------------------------------------
+// Christmas is coming, and Santa has a long list to go through, to find who deserves presents for the big day. Go through a list of children, and return a list containing every child who appeared on Santa's list. Do not add any child more than once. Output should be sorted.
+
+// Comparison should be case sensitive and the returned list should contain only one copy of each name: "Sam" and "sam" are different, but "sAm" and "sAm" are not.
+
+
+// Solution:
+// --------------------------------------
+function findChildren(santasList, children) {
+  return [... new Set(santasList.filter(e => children.includes(e)).sort())];
+}
+
+console.log(findChildren(["Jason", "Jackson", "Jordan", "Johnny"], ["Jason", "Jordan", "Jennifer"]), ["Jason", "Jordan"])
+
+console.log(findChildren(["jASon", "JAsoN", "JaSON", "jasON"], ["JasoN", "jASOn", "JAsoN", "jASon", "JASON"]), ["JAsoN", "jASon"])
