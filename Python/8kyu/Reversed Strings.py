16a470295807b726f1cae56f81e89a27535c80bc
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-10-04 10:22:38 -0500

    Added daily challenge

---
+++ Python/8kyu/Reversed Strings.py
@@ -0,0 +1,14 @@
+# Description:
+# --------------------------------------
+# Complete the solution so that it reverses the string passed into it.
+
+# 'world'  =>  'dlrow'
+# 'word'   =>  'drow'
+
+
+# Solution:
+# --------------------------------------
+def solution(string):
+    return string[::-1]
+
+print(solution("hello"), "olleh")
