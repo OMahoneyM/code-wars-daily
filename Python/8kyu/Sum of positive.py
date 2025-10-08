ee1ef922713d6ee80d17d568427fd798984da928
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-10-08 10:41:37 -0500

    Added daily challenge

---
+++ Python/8kyu/Sum of positive.py
@@ -0,0 +1,26 @@
+# Description:
+# --------------------------------------
+# Task
+
+# You get an array of numbers, return the sum of all of the positives ones.
+# Example
+
+#     [1, -4, 7, 12] => 1+7+12=20 1 + 7 + 12 = 20 1+7+12=20
+
+# Note
+
+# If there is nothing to sum, the sum is default to 0.
+
+
+# Solution:
+# --------------------------------------
+def positive_sum(arr):
+    sum = 0
+    
+    for val in arr:
+        if val >= 0:
+            sum += val
+    
+    return sum
+
+print(positive_sum([1,2,3,4,-5]), 10)
