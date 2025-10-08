f352abd2626982e0836817f01e31e25b05760a50
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-10-08 10:44:13 -0500

    Added alt solution

--- Python/8kyu/Sum of positive.py
+++ Python/8kyu/Sum of positive.py
@@ -24,3 +24,10 @@ def positive_sum(arr):
     return sum
 
 print(positive_sum([1,2,3,4,-5]), 10)
+
+# Alt Solution:
+# --------------------------------------
+def positive_sum(arr): 
+    return sum(x for x in arr if x > 0)
+
+print(positive_sum([1,2,3,4,-5]), 10)
