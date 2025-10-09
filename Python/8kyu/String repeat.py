1e7522a5fa029244c3407601c81e323fc31eb979
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-10-09 10:27:08 -0500

    Added daily challenge

---
+++ Python/8kyu/String repeat.py
@@ -0,0 +1,15 @@
+# Description:
+# --------------------------------------
+# Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+# Examples (input -> output)
+
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+# Solution:
+# --------------------------------------
+def repeat_str(repeat, string):
+    return string * repeat
+
+print(repeat_str(3, "hello"), "hellohellohello")
