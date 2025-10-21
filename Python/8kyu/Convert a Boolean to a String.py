27bbf86011c32324b1c77710db71c7eae2be4455
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-10-20 22:08:47 -0500

    Added daily challenge

---
+++ Python/8kyu/Convert a Boolean to a String.py
@@ -0,0 +1,11 @@
+# Description:
+# --------------------------------------
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+
+# Solution:
+# --------------------------------------
+def boolean_to_string(b):
+    return str(b)
