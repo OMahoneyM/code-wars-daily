9efa3d0246816ac1d6c5d2d880737ce3909860a3
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-10-31 11:42:51 -0500

    Added daily challenge

---
+++ Python/8kyu/Are You Playing Banjo?.py
@@ -0,0 +1,26 @@
+# Description:
+# --------------------------------------
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+# The function takes a name as its only argument, and returns one of the following strings:
+
+# name + " plays banjo" 
+# name + " does not play banjo"
+
+# Names given are always valid strings.
+
+
+# Solution:
+# --------------------------------------
+def are_you_playing_banjo(name):
+    if name[0].lower() == 'r':
+        return f'{name} plays banjo'
+    else:
+        return f'{name} does not play banjo'
+  
+
+# Alt Solution:
+# --------------------------------------
+def areYouPlayingBanjo(name):
+    return name + (' plays' if name[0].lower() == 'r' else ' does not play') + ' banjo'
