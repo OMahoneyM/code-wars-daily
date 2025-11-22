3d43340a29f7c3af28d8c279a6a5a1b33c5cbfa6
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-11-22 09:28:24 -0600

    Added daily challenge

---
+++ Python/8kyu/If you can't sleep, just count sheep!.py
@@ -0,0 +1,18 @@
+# Description:
+# --------------------------------------
+# Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+
+# Solution:
+# --------------------------------------
+def count_sheep(n):
+    result = ''
+    for i in range(1, n + 1):
+        result += f'{i} sheep...'
+    return result
+
+
+# Alt Solution:
+# --------------------------------------
+def count_sheep(n):
+    return ''.join(f'{i} sheep...' for i in range(1, n + 1))
