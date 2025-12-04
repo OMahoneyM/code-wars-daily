dfee42cbcb717c9533ec1cba08a5391b1da7af42
Michael O'Mahoney <mjvomahoney@gmail.com>
2025-12-04 09:53:53 -0600

    Added daily challenge

---
+++ Python/7kyu/List Filtering.py
@@ -0,0 +1,16 @@
+# Description:
+# --------------------------------------
+# In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+# Example
+
+# filter_list([1,2,'a','b']) == [1,2]
+# filter_list([1,'a','b',0,15]) == [1,0,15]
+# filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+
+# Solution:
+# --------------------------------------
+def filter_list(l):
+    return [num for num in l if type(num) is int]
+
+print(filter_list([1, 2, 3, "a", "b"]), [1, 2, 3])
